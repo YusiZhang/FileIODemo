9c822d6f8ca43f218d3a23811e07c9044d2a8694
YusiZhang <yusi.leo.zhang@gmail.com>
2014-02-12 16:58:54 -0500

    ini demo

--- src/FileDemo.java
+++ src/FileDemo.java
@@ -1,8 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class FileDemo {
 
-	public static void main(String[] args) {
+	private static void writeHTMLToFile(String file, String url) throws Exception{
+		URL testurl = new URL(url);
+		BufferedReader in = new BufferedReader(new InputStreamReader(testurl.openStream()));
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File("src/"+file+".html")));
+		String nextline;
+		while ((nextline=in.readLine()) != null) {
+			out.write(nextline);
+		}
 		
+		in.close();
+		out.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		writeHTMLToFile("test", "https://www.google.com");
 
 	}
 
